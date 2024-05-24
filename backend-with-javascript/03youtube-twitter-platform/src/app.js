8c167a57c5e5da7af48ef7bc210dd0dc44a2b0ec
saagarbisht <sgrcoc3@gmail.com>
2024-05-24 22:14:40 +0530

    set up ready

--- backend-with-javascript/03youtube-twitter-platform/src/app.js
+++ backend-with-javascript/03youtube-twitter-platform/src/app.js
@@ -8,10 +8,15 @@ app.use(cors({
     origin : process.env.CORS_ORIGIN
 }))
 
-app.use(json({limit : "16kb"}));
+app.use(express.json({limit : "16kb"}));
 app.use(urlencoded({extended : true, limit : "16kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+// Routes
+
+import userRouter from "./routes/user.routes.js"
+
+app.use("/api/v1/users",userRouter);
 
 export  {app};
